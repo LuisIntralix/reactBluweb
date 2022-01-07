8d5d4f24b2b96a625a9f2dce4a19b09d59b33775
Luis Eduardo Rodriguez <lrodriguez@intralix.com>
2022-01-06 23:06:33 -0600

    Test RGC

---
+++ pruebaRGC/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+import Menu from './components/Menu'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Menu/>
+    
+    </div>
+  )
+}
+
+export default App
---
+++ pruebaRGC/src/components/MyContent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MyContent = () => {
+    return (
+        <>
+           <h1>Hola webos</h1>
+        </>
+    )
+}
+
+export default MyContent
